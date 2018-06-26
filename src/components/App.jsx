5cfb6ab7cb859d022c9509999740fbc95c26be5e
Isaac Shoman and Mike Chu <isaac+mike@>
2018-06-26 11:52:06 -0700

    Add routing to template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 
 function App(){
   return (
     <div>
       <h1>Hello!</h1>
+      <Switch>
+        // <Route exact path='/' component={TicketList} /> //Add route here using this sytax
+      </Switch>
     </div>
   );
 }
